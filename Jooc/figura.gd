d795bcbaca221c07f725ceebfe8162c64a3bff31
Bernati05 <101257644+Bernati05@users.noreply.github.com>
2023-03-16 09:47:10 +0100

    continuació joc

--- Jooc/figura.gd
+++ Jooc/figura.gd
@@ -1,28 +1,29 @@
 extends KinematicBody2D
-var velocitat := 100
+var velocitat := 200
 var moviment := Vector2.ZERO
-var gravetat := Vector2.DOWN * 100
+var gravetat := Vector2.DOWN * 300
 var direccio := Vector2.ZERO
+var salt := 270
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	position = Vector2(0, 0)
-	scale = Vector2(1,1)
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	moviment += gravetat * delta
+	moviment.x = 0
+	direccio.x = 0
+	
 	if Input.is_action_pressed("ves_dreta"):
 		direccio += Vector2.RIGHT
 	if Input.is_action_pressed("ves_esq"):
 		direccio += Vector2.LEFT
-	if Input.is_action_pressed("baixa"):
-		direccio += Vector2.DOWN
-	if Input.is_action_pressed("puja"):
-		direccio += Vector2.UP
+	if Input.is_action_pressed("puja") and is_on_floor():
+		moviment.y = -salt
 	
 	move_and_slide(direccio.normalized() * velocitat)
 	moviment += gravetat * delta
-	move_and_slide(moviment)
+	
+	moviment = move_and_slide(moviment, Vector2.UP)
+	
